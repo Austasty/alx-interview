5f55bee546801edc3c391f080c0fa19ea22d4e6f
Austasty <obuesiogonna2@gmail.com>
2024-06-09 14:56:12 +0100

    added new files

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,30 +2,44 @@
 
 const request = require('request');
 
-const movieId = process.argv[2];
-const movieEndpoint = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
-
-function sendRequest (characterList, index) {
-  if (characterList.length === index) {
-    return;
-  }
-
-  request(characterList[index], (error, response, body) => {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log(JSON.parse(body).name);
-      sendRequest(characterList, index + 1);
-    }
-  });
+if (process.argv.length < 3) {
+    console.log('Please provide a Movie ID as a positional argument.');
+    process.exit(1);
 }
 
-request(movieEndpoint, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else {
-    const characterList = JSON.parse(body).characters;
+const movieId = process.argv[2];
+const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
 
-    sendRequest(characterList, 0);
-  }
+request(apiUrl, { json: true }, (error, response, body) => {
+    if (error) {
+        console.error('Error fetching the movie data:', error);
+        return;
+    }
+
+    if (response.statusCode !== 200) {
+        console.error(`Failed to fetch movie data. Status code: ${response.statusCode}`);
+        return;
+    }
+
+    if (!body.characters) {
+        console.error('No characters found for this movie.');
+        return;
+    }
+
+    const characters = body.characters;
+    characters.forEach((characterUrl) => {
+        request(characterUrl, { json: true }, (error, response, body) => {
+            if (error) {
+                console.error('Error fetching character data:', error);
+                return;
+            }
+
+            if (response.statusCode !== 200) {
+                console.error(`Failed to fetch character data. Status code: ${response.statusCode}`);
+                return;
+            }
+
+            console.log(body.name);
+        });
+    });
 });
